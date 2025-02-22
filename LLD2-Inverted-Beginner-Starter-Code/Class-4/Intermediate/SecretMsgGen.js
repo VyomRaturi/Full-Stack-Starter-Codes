a83ecaf65e9154d3dd546e429406863cf96f3965
Vyom <vyomraturi2004@gmail.com>
2025-02-22 15:13:29 +0530

    added class 4 (basic js syntax) to lld2 beginner

---
+++ LLD2-Inverted-Beginner-Starter-Code/Class-4/Intermediate/SecretMsgGen.js
@@ -0,0 +1,11 @@
+// Implement secretMessage(secret) that:
+//     Returns a function.
+//     When called, returns the stored secret.
+
+// Approach: Think about what this function will return. Will you get a value when you call this function or something that can be called later. 
+
+const secret = secretMessage("Hidden Code");
+
+function secretMessage(secret) {
+  // Write your code here
+}
